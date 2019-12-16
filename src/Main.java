4c13234a66f94bd77cea31be1271d308b895e7ae
Matheus Paro Ferreira <37188919+SonicDog51@users.noreply.github.com>
2019-12-16 12:30:33 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,25 +4,41 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
+        for (int i =0; i<20;i++) {
+            System.out.println(string_Generator());
+        }
+    }
+
+    public static String string_Generator(){
         String emailAddress = "john.doe.mis2016@ie.edu";
-        String formattedEmailAddress = emailAddress.replaceAll(".", "/");
+        String formattedEmailAddress = emailAddress.replaceAll("\\.", "/");
 
         Random random = new Random();
         StringBuffer word = null;
-
-        switch (random.nextInt(2)) {
+        int number = random.nextInt(3);
+        switch (number) {
             case 0:
-                word = new StringBuffer('Y');
+                word = new StringBuffer();
+                word.append('Y');
+                break;
             case 1:
-                word = new StringBuffer('F');
+                word = new StringBuffer();
+                word.append('T');
+                break;
             case 2:
-                word = new StringBuffer('T');
+                word = new StringBuffer();
+                word.append('F');
+                break;
+            default:
+                System.out.println("this shouldn't appear");
         }
 
         word.append('o');
         word.append('u');
         word.append('r');
 
-        System.out.println(word + " " + formattedEmailAddress);
+        return word + " " + formattedEmailAddress;
+
+
     }
 }
